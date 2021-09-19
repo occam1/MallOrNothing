63d30c58842d1a960ea224a5303c63c56c96e3cd
Mark <mark_a_hall@yahoo.com>
2021-09-19 13:05:09 -0500

    initial load

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StoreComponent } from './store/store.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StoreComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
